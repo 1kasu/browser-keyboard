7a1cbdd322c75192222218d897b5d7c8d1a9dab0
Kasimir Jyry Santeri Ilmonen <jyrysanteri@hotmail.com>
2020-10-02 02:38:00 +0300

    Lisatty ruudukko

--- src/App.js
+++ src/App.js
@@ -3,16 +3,35 @@ import './App.css';
 
 function Nappi(props) {
   return <button onClick={()=> alert("Auts!")}>
-    Nappi {props.name}
+    {props.name}
   </button>;
 }
 
+function NumeroSyote(props) {
+  return <input type="number" id={props.id} name={props.id} min={props.min} max={props.max} value={props.value}/>
+}
+
+function Ruudukko(props) {
+  var i = 1;
+  var ruudut = [];
+  for (var l = 0; l < props.leveys; l++){
+    ruudut.push(<div class="ruutu">{i++}</div>);
+  }
+  return <div class="ruudukko">{ruudut}</div>;
+}
+
 function App() {
+  var leveys = 40;
+  var korkeus = 2;
   return (
     <div className="App">
       <h1>Browser Keyboard</h1>  
-      <Nappi name="A"/>
-      <Nappi name="B"/>
+      <label for="leveysSyote">Leveys</label>
+      <NumeroSyote id="leveysSyote" min="1" max="10" value={leveys}/>
+      <label for="korkeusSyote">Korkeus</label>
+      <NumeroSyote id="korkeusSyote" min="1" max="10"value={korkeus}/>
+      <Nappi name="Päivitä"/>
+      <Ruudukko leveys={leveys} korkeus={korkeus}/>
     </div>
   );
 }
