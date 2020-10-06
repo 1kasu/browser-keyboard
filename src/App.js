3b3430e63807ca0356644cc443775e4ec5476dba
Kasimir Jyry Santeri Ilmonen <jyrysanteri@hotmail.com>
2020-10-06 22:58:48 +0300

    Ruudukon kokoa voi muuttaa

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function Nappi(props) {
-  return <button onClick={()=> alert("Auts!")}>
+  return <button onClick={props.onClick}>
     {props.name}
   </button>;
 }
@@ -11,7 +11,9 @@ function NumeroSyote(props) {
   return <input type="number" id={props.id} name={props.id} min={props.min} max={props.max} value={props.value}/>
 }
 
-function Ruudukko(props) {
+
+
+function Ruudukko(props){
   var i = 1;
   var ruudut = [];
   for (var l = 0; l < props.leveys; l++){
@@ -20,20 +22,37 @@ function Ruudukko(props) {
   return <div class="ruudukko">{ruudut}</div>;
 }
 
-function App() {
-  var leveys = 40;
-  var korkeus = 2;
-  return (
-    <div className="App">
-      <h1>Browser Keyboard</h1>  
-      <label for="leveysSyote">Leveys</label>
-      <NumeroSyote id="leveysSyote" min="1" max="10" value={leveys}/>
-      <label for="korkeusSyote">Korkeus</label>
-      <NumeroSyote id="korkeusSyote" min="1" max="10"value={korkeus}/>
-      <Nappi name="Päivitä"/>
-      <Ruudukko leveys={leveys} korkeus={korkeus}/>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      leveys: 30,
+
+    };
+  }
+
+  paivitaRuudukko(){
+    //alert("Päivitetään ruudukon koko");
+    this.setState({
+      leveys: this.state.leveys - 1, 
+    });
+  }
+
+  render(){
+    var korkeus = 2;
+    return (
+      <div className="App">
+        <h1>Browser Keyboard</h1>  
+        <label for="leveysSyote">Leveys</label>
+        <NumeroSyote id="leveysSyote" min="1" max="100" value={this.state.leveys}/>
+        <label for="korkeusSyote">Korkeus</label>
+        <NumeroSyote id="korkeusSyote" min="1" max="100"value={korkeus}/>
+        <Nappi onClick={() => this.paivitaRuudukko()} name="Päivitä"/>
+        <Ruudukko leveys={this.state.leveys} korkeus={korkeus}/>
+      </div>
+    );
+  }
 }
 
 export default App;
